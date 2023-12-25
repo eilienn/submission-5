7f8fc6484677ba689d77ec5ce414e2f07bd5257d
eilienn <79395572+eilienn@users.noreply.github.com>
2023-12-25 13:05:12 +0700

    Add files via upload

---
+++ database/migrations/2023_12_11_082529_create_pemilik_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemilik', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('NamaDepan');
+            $table->string('NamaBelakang');
+            $table->string('harga');
+            $table->string('jenis');
+            $table->string('kota');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemilik');
+    }
+};
